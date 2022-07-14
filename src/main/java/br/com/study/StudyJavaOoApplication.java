17da89d3e3bfa9751f9f11ae4df03cffd4d4649e
carolina <anacarolinavgoncalves@gmail.com>
2022-07-14 12:46:43 -0300

    start project carol

---
+++ src/main/java/br/com/study/StudyJavaOoApplication.java
@@ -0,0 +1,13 @@
+package br.com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJavaOoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJavaOoApplication.class, args);
+	}
+
+}
